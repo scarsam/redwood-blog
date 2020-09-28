c0f3a83b7a735dcdc8ab5a638e5fad6cd1b23192
sam <s.ojling@gmail.com>
2020-09-28 10:21:24 +0200

    Continue on the tutorial

---
+++ web/src/pages/AboutPage/AboutPage.js
@@ -0,0 +1,12 @@
+import BlogLayout from 'src/layouts/BlogLayout'
+
+const AboutPage = () => (
+  <BlogLayout>
+    <p>
+      This site was created to demonstrate my mastery of Redwood: Look on my
+      works, ye mighty, and despair!
+    </p>
+  </BlogLayout>
+)
+
+export default AboutPage
